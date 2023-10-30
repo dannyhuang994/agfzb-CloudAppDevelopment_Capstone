53509610db157039817b690d67c64a911621fcce
dannyhuang994 <dannyhuang994@gmail.com>
2023-10-30 15:01:58 -0400

    added contact page and about page

--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -18,23 +18,29 @@ logger = logging.getLogger(__name__)
 
 
 # Create an `about` view to render a static about page
-# def about(request):
-# ...
+def about(request):
+    context = {}
+    return render(request, "djangoapp/about.html", context)
 
 
 # Create a `contact` view to return a static contact page
-#def contact(request):
+def contact(request):
+    context = {}
+    return render(request, "djangoapp/contact.html", context)
 
 # Create a `login_request` view to handle sign in request
-# def login_request(request):
+def login_request(request):
+    pass
 # ...
 
 # Create a `logout_request` view to handle sign out request
-# def logout_request(request):
+def logout_request(request):
+    pass
 # ...
 
 # Create a `registration_request` view to handle sign up request
-# def registration_request(request):
+def registration_request(request):
+    pass
 # ...
 
 # Update the `get_dealerships` view to render the index page with a list of dealerships
@@ -45,10 +51,12 @@ def get_dealerships(request):
 
 
 # Create a `get_dealer_details` view to render the reviews of a dealer
-# def get_dealer_details(request, dealer_id):
+def get_dealer_details(request, dealer_id):
+    pass
 # ...
 
 # Create a `add_review` view to submit a review
-# def add_review(request, dealer_id):
+def add_review(request, dealer_id):
+    pass
 # ...
 
